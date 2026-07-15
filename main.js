f6cc12ed6e4c42a64e194c6ac45a6acd460bca72
divyanshu <divyanshusah44@gmail.com>
2026-07-15 16:59:35 +0530

    creating tasks with fs module

--- main.js
+++ main.js
@@ -11,15 +11,22 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"public")))
 
 
-app.get("/",(req,res)=>{
-    res.send("yoooo")
-})
-app.get("/tasks", (req, res)=>{
+
+app.get("/", (req, res)=>{
     fs.readdir(`./files`, (err,files)=>{
         res.render("index", {files: files}) 
     })
     
 })
+
+app.post("/create", (req, res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err)=>{
+        res.redirect("/")
+    })
+    
+})
+
+
 
 app.listen(PORT,()=>{
     console.log(`server is running`)
