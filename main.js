7a060b7fac111e92e4cda138d6b3556a04a3714e
divyanshu <divyanshusah44@gmail.com>
2026-07-16 00:20:25 +0530

    edit filename feature added

--- main.js
+++ main.js
@@ -14,10 +14,26 @@ app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/", (req, res)=>{
     fs.readdir(`./files`, (err,files)=>{
-        res.render("index", {files: files}) 
+        res.render("index", {varfiles: files}) 
     })
     
 })
+
+app.get("/file/:filename", (req, res)=>{
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata)=>{
+        res.render("show", {filename: req.params.filename, filedata:filedata})
+    })   
+})
+
+app.get("/edit/:filename", (req, res)=>{
+        res.render("edit" , {filename: req.params.filename})
+    })  
+
+app.post("/edit", (req, res)=>{
+        fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`,(err)=>{
+            res.redirect("/")
+        })
+    })  
 
 app.post("/create", (req, res)=>{
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err)=>{
@@ -25,6 +41,7 @@ app.post("/create", (req, res)=>{
     })
     
 })
+
 
 
 
